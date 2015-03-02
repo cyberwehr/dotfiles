ba86eb40c9dca7a228b5cfc0ba9726e626338d3e
Steven Schaap <s.schaap@sportfotos-salzlandkreis.de>
2015-03-02 19:10:58 +0100

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+read -p "Bitte den neuen Hostnamen eingeben: " hostname
+echo > /etc/bash.bashrc
+echo $hostname > /etc/hostname
+wget --no-check-certificate -O /root/.bashrc https://raw.githubusercontent.com/tz0/dotfiles/master/nas.bashrc
+apt-get update
+apt-get -y upgrade
+apt-get -y install htop nload unzip zip nmap curl
+reboot
