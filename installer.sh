a6489bcce68edbcac18300997643a3f42d132145
Steven Schaap <s.schaap@sportfotos-salzlandkreis.de>
2015-03-18 16:48:29 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -5,5 +5,5 @@ echo $hostname > /etc/hostname
 wget --no-check-certificate -O /root/.bashrc https://raw.githubusercontent.com/tz0/dotfiles/master/nas.bashrc
 apt-get update
 apt-get -y upgrade
-apt-get -y install htop nload unzip zip nmap curl
+apt-get -y install htop nload unzip zip nmap curl mtr
 reboot
